e733501059e2cfe17c5ed663421dd08fbab8fca6
carbonecar <carbonecar@gmail.com>
2023-02-10 12:55:21 -0300

    initial commit

---
+++ lib/src/main/java/com/jobint/ingreso/sorting/Sorting.java
@@ -0,0 +1,33 @@
+package com.jobint.ingreso.sorting;
+
+import java.util.List;
+
+public class Sorting {
+
+    /**
+     * Se debe ordenar primero por puntuación de manera descendente, luego por
+     * nombre de manera ascendente.
+     * 
+     * @param jugadores la lista de jugadores a ordenar
+     * @return la lista ordenada de jugadores
+     */
+    public static List<Jugador> ordenarPorPuntuacionYNombre(List<Jugador> jugadores) {
+        // TODO: implementar el ordenamiento
+        return jugadores;
+    }
+
+    /**
+     * Se debe ordenar primero por puntuación de manera descendente. Cuando 2
+     * jugadores tienen igual cantidad de puntos,
+     * el que tiene menos perdidas se lo considerara el mayor. Luego a igual puntos
+     * y perdidas se seguirá usando el
+     * nombre de manera ascendente.
+     * 
+     * @param jugadores la lista de jugadores a ordenar
+     * @return la lista ordenada de jugadores
+     */
+    public static List<Jugador> ordenarPorPuntuacionPerdidasYNombre(List<Jugador> jugadores) {
+        // Implementar el ordenamiento
+        return jugadores;
+    }
+}
